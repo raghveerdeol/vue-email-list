56a557e4b6e612c6474e61d4998aa8bd0e44c70a
raghveerdeol <deolraghveer5@gmail.com>
2024-06-05 23:01:53 +0200

    visuallizzato in pagina 2 array oggetto, 1 con email diverse e l'altro con tutte email uguali

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ createApp({
     data() {
         return {
             emails: '',
-            
+            datas: ''
         }
     },
     methods:{
-        getEmail: () => {
+        getEmail() {
             let data = [];
             for (let index = 0; index < 10; index++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail ').then(function (response) {
@@ -17,9 +17,22 @@ createApp({
             }
             this.emails = data;
             console.log(data, this.emails)
+        },
+        getData() {
+            let x = 0;
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail ').then((response) => {
+                let data = [];
+                while (x < 10) {
+                data.push(response.data.response);
+                x++;
+                }
+                this.datas = data;
+                console.log(data)
+            })
         }
     },
     created(){
         this.getEmail();
-    }
+        this.getData();
+    },
 }).mount('#app')

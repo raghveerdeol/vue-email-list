282d2b8d1fda01b8944cdd4deca4b4756dbf5f2b
raghveerdeol <deolraghveer5@gmail.com>
2024-06-06 21:40:51 +0200

    aggiunto arrow function e v-if

--- js/script.js
+++ js/script.js
@@ -3,36 +3,20 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            emails: '',
-            datas: ''
+            emails: [],
         }
     },
     methods:{
         getEmail() {
-            let data = [];
             for (let index = 0; index < 10; index++) {
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail ').then(function (response) {
-                    data.push(response.data.response);
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail ').then( (response) => {
+                    this.emails.push(response.data.response);
                 })
             }
-            this.emails = data;
-            console.log(data, this.emails)
         },
-        getData() {
-            let x = 0;
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail ').then((response) => {
-                let data = [];
-                while (x < 10) {
-                data.push(response.data.response);
-                x++;
-                }
-                this.datas = data;
-                console.log(data)
-            })
-        }
     },
     created(){
         this.getEmail();
-        this.getData();
+
     },
 }).mount('#app')
